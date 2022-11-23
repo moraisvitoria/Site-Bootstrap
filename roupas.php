4ebc98dfcab2c0651b713ee4deccc4d476683a9e
DrielySantos <santosdriely98@gmail.com>
2022-11-23 16:44:08 -0300

    introduzindo php

---
+++ roupas.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'head.php';
+require_once 'menu.php';
+
+?>
+
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center tela">
+                <img src="imagens/banner.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid texto">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h2>Conheça nossa loja!</h2>
+          </div>
+        </div>
+      </div>
+
+ <!--   <div class="container-fluid img-loja">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="imagens/blusa-1.webp" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/roupa-2.webp" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/roupa-3.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>-->  
+
+    <div class="container-fluid text-center conteudo-loja">
+        <div class="row">
+          <div class="col-md-6 col-sm">
+            <img src="imagens/blusa-1.webp" class="img-fluid">
+          </div>
+          <div class="col-md-6 col-sm">
+            <h3>Blusa Mullet</h3>
+              
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid text-center conteudo-loja">
+        <div class="row">
+          <div class="col-md-6 col-sm">
+            <img src="imagens/roupa-2.webp" class="img-fluid">
+          </div>
+          <div class="col-md-6 col-sm">
+            <h3>Conjunto de desporto</h3>
+              
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid text-center conteudo-loja">
+        <div class="row">
+          <div class="col-md-6 col-sm">
+            <img src="imagens/roupa-3.jpg" class="img-fluid">
+          </div>
+          <div class="col-md-6 col-sm">
+            <h3>Short verão</h3>
+              
+          </div>
+        </div>
+      </div>
+
+ <?php
+ require_once 'footer.php';
+ ?>
---
+++ suplementos.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'head.php';
+require_once 'menu.php';
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require_once 'footer.php';
+?>
