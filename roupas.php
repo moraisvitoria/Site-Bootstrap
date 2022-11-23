b0b2412bbcb46f924f65dd77ca30e0b6f57e311f
DrielySantos <santosdriely98@gmail.com>
2022-11-23 20:35:29 -0300

    estilizando page

--- roupas.php
+++ roupas.php
@@ -1,17 +1,8 @@
 <?php
 require_once 'head.php';
 require_once 'menu.php';
-
 ?>
 
-<div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 text-center tela">
-                <img src="imagens/banner.jpg" class="img-fluid">
-            </div>
-
-        </div>
-    </div>
 
     <div class="container-fluid texto">
         <div class="row">
@@ -21,29 +12,32 @@ require_once 'menu.php';
         </div>
       </div>
 
- <!--   <div class="container-fluid img-loja">
+    <div class="container-fluid img-roupa">
         <div class="row">
             <div class="col-md-4">
                 <img src="imagens/blusa-1.webp" class="img-fluid">
+                <h3>Blusa Mullet</h3>
             </div>
 
             <div class="col-md-4">
                 <img src="imagens/roupa-2.webp" class="img-fluid">
+                <h3>Conjunto de desporto</h3>
             </div>
 
             <div class="col-md-4">
                 <img src="imagens/roupa-3.jpg" class="img-fluid">
+                <h3>Short verão</h3>
             </div>
 
         </div>
-    </div>-->  
+    </div>
 
-    <div class="container-fluid text-center conteudo-loja">
+  <div class="container-fluid text-center conteudo-loja">
         <div class="row">
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <img src="imagens/blusa-1.webp" class="img-fluid">
           </div>
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <h3>Blusa Mullet</h3>
               
           </div>
@@ -52,10 +46,10 @@ require_once 'menu.php';
 
       <div class="container-fluid text-center conteudo-loja">
         <div class="row">
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <img src="imagens/roupa-2.webp" class="img-fluid">
           </div>
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <h3>Conjunto de desporto</h3>
               
           </div>
@@ -64,10 +58,10 @@ require_once 'menu.php';
 
       <div class="container-fluid text-center conteudo-loja">
         <div class="row">
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <img src="imagens/roupa-3.jpg" class="img-fluid">
           </div>
-          <div class="col-md-6 col-sm">
+          <div class="col-md-4 col-sm">
             <h3>Short verão</h3>
               
           </div>
