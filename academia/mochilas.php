0c8eab11c4c359ac8890dd3fd7cef6b3653cc9e2
Rafaela Vitória <rafaelavitoria21@gmail.com>
2022-11-30 16:28:11 -0300

    atualizaçao past academia

--- academia/mochilas.php
+++ academia/mochilas.php
@@ -59,7 +59,7 @@
 
             <div class="col-md-3">
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="imagens/mochila4.jpg">
+                    <img class="card-img-top"src="imagens/mochila4.jpg">
                     <div class="card-body">
                     <h3 class="card-text">Mochila Academia</h3>
                         <h4>R$ 80,00</h4>
@@ -104,6 +104,79 @@
     </div>
   </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="Raissa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="mochila Raissa" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        ...
+        <img class="card-img-fluid" src="imagens/mochila2.jpg">
+        <p>Mochila de couro, Moderna , Bem estruturada.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicioanar ao carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="Mayra" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="mochila Mayra" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        ...
+        <img class="card-img-fluid" src="imagens/mochila3.jpg">
+        <p>Mochila de Napa, confortável , Bem estruturada.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicioanar ao carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="Academia" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="mochila Academia" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        ...
+        <img class="card-img-fluid" src="imagens/mochila4.jpg">
+        <p>Mochila de Napa, pratica para passear, Bem estruturada.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicioanar ao carrinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
    
     <?php
         require_once 'footer.php';
