972cbb71103f9769fd43ca5eb930ee0e1a6a3d9f
DrielySantos <santosdriely98@gmail.com>
2022-12-01 16:50:46 -0300

    pag suplementos

--- menu.php
+++ menu.php
@@ -1,5 +1,7 @@
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Academia</a>
+        <a class="navbar-brand" href="index.php">We Fitness</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -7,10 +9,10 @@
         <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(página atual)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Nossa Academia</a>
+              <a class="nav-link" href="about.php">Nossa Academia</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
