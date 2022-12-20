ecf39867fd91289c413ad19e88d561323a994473
DrielySantos <santosdriely98@gmail.com>
2022-12-20 11:04:09 -0300

    form css

--- formulario.php
+++ formulario.php
@@ -2,86 +2,130 @@
 require_once 'head.php';
 ?>
 
-<form>
-  <div class="container-fluid">
-    <div class="form-row">
-     <div class="col-md-4 mb-3">
-      <label for="validationServer01">Nome</label>
-      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Nome" value="vazio" required>
-      <div class="valid-feedback">
-        Tudo certo!
-      </div>
-    </div>
-    
-    <div class="form-group">
-       <label for="telefone">Telefone</label>
-       <input type="text" class="form-control" onkeypress="$(this).mask('(00)00000-0000')">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1>Formulário do Aluno</h1>
     </div>
   </div>
-    
+</div>
 
-    <div class="row">
+<form>
+
+    <div class="container form-aluno">
+      <div class="row">
         <div class="col-md-4">
-          <div class="form-grupo">
+          <div class="form-group">
+              <label for="nome">Nome</label>
+              <input type="text" class="form-control" placeholder="Seu nome">    
+          </div>
+        </div>        
+            
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="nome">Sobrenome</label>
+            <input type="text" class="form-control" placeholder="Seu sobrenome">    
+          </div>
+        </div>       
+        
+        <div class="col-md-4">        
+          <div class="form-group">            
             <label for="dn">Data de Nascimento</label>
             <input type="date" class="form-control">
+          </div>
         </div>
     </div>
-
-    <div class="col-md-4">
-        <div class="form-grupo">
-            <label for="sexo">Sexo</label>
-            <p><input type="radio">Feminino
-            <input type="radio">Masculino</p>
-        </div>
-       </div>
-    </div>
-
-
-        <div class="form-group">
-          <div class="col-md-4">
-            <label for="cpf">CPF</label>
-            <input type="number" name="cpf" \
-			   pattern="\d{3}\.?\d{3}\.?\d{3}-?\d{2}" \
-			   title="Digite um CPF no formato: xxx.xxx.xxx-xx">
-		    <input type="submit" value="Verificar">
+        
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="telefone">Telefone</label>
+            <input type="text" class="form-control" onkeypress="$(this).mask('(00)00000-0000')">
           </div>
         </div>
 
-    <div class="col-md-3 mb-3">
-      <label for="validationServer05">CEP</label>
-      <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="CEP" required>
-      <div class="invalid-feedback">
-        Por favor, informe um CEP válido.
+        <div class="col-md-4">        
+          <div class="form-group">            
+            <label for="cpf">CPF</label>
+            <input type="text" class="form-control" onkeypress="$(this).mask('000.000.000-00');">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="rg">RG</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="cep">CEP</label>
+            <input type="text" class="form-control" onkeypress="$(this).mask('00000-000');">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="endereco">Endereço</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="col-md-2">
+          <div class="form-group">
+            <label for="numerocasa">N° Casa</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="complemento">Complemento</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="form-group end-form">
+            <label for="exampleInputEmail1">Endereço de email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+            <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+          </div>
+        </div>
+
+        <div class="col-md-2">
+          <div class="form-group end-form">
+            <label for="exampleInputPassword1">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+           </div>
+        </div>
+      </div>
+      
+    <div class="row">
+      <div class="col-md-4">
+        <div class="form-group">
+          <label for="sexo">Sexo</label><p>
+          <input type="radio" name="optradio" checked> Feminino           
+          <input type="radio" name="optradio"> Masculino
+        </div>
       </div>
     </div>
 
-  <div class="form-row">
-    <div class="col-md-3 mb-3">
-      <label for="validationServer03">Cidade</label>
-      <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="Cidade" required>
-      <div class="invalid-feedback">
-        Por favor, informe uma cidade válida.
-      </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
     </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationServer04">Estado</label>
-      <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="Estado" required>
-      <div class="invalid-feedback">
-        Por favor, informe um estado válido.
-      </div>
-    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
   </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-      <label class="form-check-label" for="invalidCheck3">
-        Concordo com os termos e condições
-      </label>
-      <div class="invalid-feedback">
-        Você deve concordar, antes de continuar.
-      </div>
-    </div>
-  </div>
-  <button class="btn btn-primary" type="submit">Enviar</button>
+  
 </form>
+
+<?php
+require_once 'footer.php';
+?>
