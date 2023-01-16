88bd83ede013f2cf40cf2bbb8eb81231b328077a
Rafaela Vitória <rafaelavitoria21@gmail.com>
2023-01-16 16:56:00 -0300

    now2

--- controlealuno.php
+++ controlealuno.php
@@ -1,12 +1,39 @@
 <?php
 include_once 'conexao.php' ;
 
-$dadoscad = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-if (!empty($dadoscad['btncad'])) {
 
-    $sql = "insert into aluno("nome, sexo,datadenascimento,telefone,cpf,rg,cep,numerocasa,complemento,foto,email,senha)
-    values()";
+try{
+$dadoscad = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 }
 
+if (!empty($dadoscad['btncad'])) {
 
+    $sql = "insert into aluno(nome, sexo,datadenascimento,telefone,cpf,rg,cep,numerocasa,complemento,foto,email,senha)
+    values(:nome,:sexo,:datadenascimento,:telefone,:cpf,:rg,:cep,:numerocasa,:complemento,:foto,:email,:senha)";
+    
+    $salvar= $conn->prepare($sql);
+    $salvar -> bindParam (':nome', $dadoscad['nome'],PDO::PARM_STR);
+    $salvar -> bindParam (':sexo', $dadoscad['sexo'],PDO::PARM_STR);
+    $salvar -> bindParam (':datadenascimento', $dadoscad['datadenascimento'],PDO::PARM_STR);
+    $salvar -> bindParam (':telefone', $dadoscad['telefone'],PDO::PARM_STR);
+    $salvar -> bindParam (':cpf', $dadoscad['cpf'],PDO::PARM_STR);
+    $salvar -> bindParam (':rg', $dadoscad['rg'],PDO::PARM_STR);
+    $salvar -> bindParam (':cep', $dadoscad['cep'],PDO::PARM_STR);
+    $salvar -> bindParam (':numerodecasa', $dadoscad['numero'],PDO::PARM_INT);
+    $salvar -> bindParam (':compleneto', $dadoscad['complemento'],PDO::PARM_STR);
+    $salvar -> bindParam (':foto', $dadoscad['foto'],PDO::PARM_STR);
+    $salvar -> bindParam (':email', $dadoscad['email'],PDO::PARM_STR);
+    $salvar -> bindParam (':senha', $dadoscad['senha'],PDO::PARM_STR);
+
+    if ($salvar->rowCount()) {
+        echo "Usuário cadastrado com sucesso!</p>";
+        unset($dadoscad);
+    }else{
+        echo"usuário não cadastrado com sucesso"!</p>";
+    }
+}
+}
+cath(PDOException $erro){
+    echo $erro;
+}
 ?>
--- formulario.php
+++ formulario.php
@@ -17,29 +17,29 @@ require_once 'menu.php';
       <div class="row">
         <div class="col-md-5">
           <div class="form-group">
-            <label for="nome">Nome e sobrenome</label>
+            <label for="nome">nome e sobrenome</label>
             <input type="text" class="form-control" name="nome" placeholder="Seu nome e sobrenome">    
           </div>
         </div>      
         
         <div class="col-md-2">        
           <div class="form-group">            
-            <label for="dn">Data de Nascimento</label>
+            <label for="dn">data de nascimento</label>
             <input type="date" class="form-control" name="dn">
           </div>
         </div>
 
         <div class="col-md-2">
           <div class="form-group">
-            <label for="telefone">Telefone</label>
-            <input type="text" class="form-control" onkeypress="$(this).mask('(00)00000-0000')" name="Telefone">
+            <label for="telefone">telefone</label>
+            <input type="text" class="form-control" onkeypress="$(this).mask('(00)00000-0000')" name="telefone">
           </div>
         </div>
 
         <div class="col-md-3">        
           <div class="form-group">            
-            <label for="cpf">CPF</label>
-            <input type="text" class="form-control" onkeypress="$(this).mask('000.000.000-00');" name="CPF">
+            <label for="cpf">cpf</label>
+            <input type="text" class="form-control" onkeypress="$(this).mask('000.000.000-00');" name="cpf">
           </div>
         </div>
   </div>
@@ -48,14 +48,14 @@ require_once 'menu.php';
     <div class="col-md-4">
       <div class="form-group">
         <label for="rg">RG</label>
-        <input type="text" class="form-control" name="RG">
+        <input type="text" class="form-control" name="rG">
       </div>
     </div>
 
     <div class="col-md-4">
       <div class="form-group">
-        <label for="exampleInputEmail1">Endereço de email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" name="Endereço de Email">                   
+        <label for="exampleInputEmail1">endereço de email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" name="endereço de Email">                   
       </div>
     </div>
 
@@ -71,29 +71,37 @@ require_once 'menu.php';
   <div class="row">
     <div class="col-md-2">
       <div class="form-group">
-        <label for="cep">CEP</label>
+        <label for="cep">cep</label>
         <input type="text" name="cep" class="form-control" id="cep" onblur="pesquisacep(this.value);" onkeypress="$(this).mask('00000-000');" name="cep">
       </div>
     </div>
 
     <div class="col-md-4">
       <div class="form-group">
-        <label for="endereco">Endereço</label>
+        <label for="endereco">endereço</label>
         <input type="text" class="form-control" id="rua" name="rua">
       </div>
     </div>
 
     <div class="col-md-2">
       <div class="form-group">
-        <label for="numero">Número</label>
-        <input type="text" class="form-control" name="Número">    
+        <label for="numero">numero</label>
+        <input type="text" class="form-control" name="numero">    
       </div>
     </div> 
 
     <div class="col-md-4">
       <div class="form-group">
-        <label for="complemento">Complemento</label>
-        <input type="text" class="form-control" placeholder="Opcional" name="Complemento">
+        <label for="complemento">complemento</label>
+        <input type="text" class="form-control" placeholder="Opcional" name="complemento">
+      </div>
+    </div>
+  </div>
+
+   <div class="col-md-4">
+      <div class="form-group">
+        <label for="complemento">foto</label>
+        <input type="text" class="form-control" placeholder="Opcional" name="foto">
       </div>
     </div>
   </div>
@@ -101,22 +109,30 @@ require_once 'menu.php';
   <div class="row">
     <div class="col-md-3">
       <div class="form-group">
-        <label for="bairro">Bairro</label><p>
+        <label for="bairro">bairro</label><p>
         <input type="text" class="form-control" id="bairro" name="bairro" >
       </div>
     </div>
 
     <div class="col-md-3">
       <div class="form-group">
-        <label for="cidade">Cidade</label><p>
-        <input type="text" class="form-control" id="cidade" name="cidade" name="Cidade">
+        <label for="cidade">cidade</label><p>
+        <input type="text" class="form-control" id="cidade" name="cidade" name="cidade">
       </div>
     </div>
 
     <div class="col-md-2  ">
       <div class="form-group">
-        <label for="uf">Estado</label><p>
-        <input type="text" class="form-control" id="uf" name="uf" name="uf">
+        <label for="uf">estado</label><p>
+        <input type="text" class="form-control"  name="estado">
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-2  ">
+      <div class="form-group">
+        <label for=>senha</label><p>
+        <input type="text" class="form-control"  name="senha" ">
       </div>
     </div>
   </div>
